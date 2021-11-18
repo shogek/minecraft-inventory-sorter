8c73d79b473e1fa7941cc302e79f56d1ae514846
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2021-11-18 19:38:33 +0200

    #2 On MMB, merge item stacks

--- src/main/java/com/example/examplemod/Server.java
+++ src/main/java/com/example/examplemod/Server.java
@@ -1,9 +1,11 @@
 package com.example.examplemod;
 
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.fmllegacy.network.NetworkEvent;
 
+import java.util.*;
 import java.util.function.Supplier;
 
 public class Server {
@@ -18,7 +20,64 @@ public class Server {
             if (player == null) {
                 return;
             }
-            player.addItem(new ItemStack(Items.DIAMOND));
+
+            var inventory = player.getInventory();
+            var airItemId = Items.AIR.getDescriptionId();
+            var itemsInInventory = new HashMap<String, List<ItemStack>>();
+
+            for (var i = InventoryMenu.INV_SLOT_START; i < InventoryMenu.INV_SLOT_END; i++) {
+                var itemStack = inventory.getItem(i);
+                var itemId = itemStack.getDescriptionId();
+
+                if (itemId.equals(airItemId)) {
+                    // Ignore empty slots
+                    continue;
+                }
+
+                if (!itemStack.isStackable()) {
+                    continue;
+                }
+
+                if (itemStack.getCount() == itemStack.getMaxStackSize()) {
+                    // Ignore maxed out stacks
+                    continue;
+                }
+
+                if (itemsInInventory.containsKey(itemId)) {
+                    var itemStacks = itemsInInventory.get(itemId);
+                    itemStacks.add(itemStack);
+                    itemsInInventory.put(itemId, itemStacks);
+                    continue;
+                }
+
+                var itemStacks = new ArrayList<ItemStack>();
+                itemStacks.add(itemStack);
+                itemsInInventory.put(itemId, itemStacks);
+            }
+
+            // Ignore single stacks
+            itemsInInventory.entrySet().removeIf((entry) -> entry.getValue().size() <= 1);
+
+            itemsInInventory.forEach((itemId, itemStacks) -> {
+                int totalItemCount = itemStacks.stream().map(ItemStack::getCount).reduce(Integer::sum).orElse(0);
+
+                for (ItemStack itemStack: itemStacks) {
+                    var maxSize = itemStack.getMaxStackSize();
+                    if (totalItemCount >= maxSize) {
+                        itemStack.setCount(maxSize);
+                        totalItemCount -= maxSize;
+                        continue;
+                    }
+
+                    if (totalItemCount > 0) {
+                        itemStack.setCount(totalItemCount);
+                        totalItemCount = 0;
+                        continue;
+                    }
+
+                    inventory.removeItem(itemStack);
+                }
+            });
         });
         context.setPacketHandled(true);
     }
