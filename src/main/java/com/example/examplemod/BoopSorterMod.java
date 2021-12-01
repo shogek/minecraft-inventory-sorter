4e22e5e5100bacafc153e7c59c6ed11bf00b9d3f
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2021-12-01 23:12:21 +0200

    #4 [Working] Sort Minecraft + IronChestMod containers

--- src/main/java/com/example/examplemod/BoopSorterMod.java
+++ src/main/java/com/example/examplemod/BoopSorterMod.java
@@ -1,8 +1,10 @@
 package com.example.examplemod;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraftforge.client.event.GuiScreenEvent;
@@ -13,6 +15,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT_CONTROL;
 import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_MIDDLE;
 
 @Mod(BoopSorterMod.MOD_ID)
@@ -33,95 +37,131 @@ public class BoopSorterMod
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class StaticKeyHandler
     {
+        /** `true` if the keyboard's CTRL key is being held down. */
+        private static boolean _isCtrlHeldDown = false;
+
+        @SubscribeEvent
+        public static void onKeyboardKeyPressedPostEvent(GuiScreenEvent.KeyboardKeyPressedEvent.Post event) {
+            if (event.getKeyCode() == GLFW_KEY_LEFT_CONTROL) {
+                StaticKeyHandler._isCtrlHeldDown = true;
+            }
+        }
+
+        @SubscribeEvent
+        public static void onKeyboardKeyReleasedPostEvent(GuiScreenEvent.KeyboardKeyReleasedEvent.Post event) {
+            if (event.getKeyCode() == GLFW_KEY_LEFT_CONTROL) {
+                StaticKeyHandler._isCtrlHeldDown = false;
+            }
+        }
+
         @SubscribeEvent
         public static void onGuiMouseReleasedEvent(GuiScreenEvent.MouseReleasedEvent.Pre event)
         {
-            if (event.getButton() != GLFW_MOUSE_BUTTON_MIDDLE)
-            {
-                Log("Ignoring because not MMB clicked.");
+            if (event.getButton() != GLFW_MOUSE_BUTTON_MIDDLE) {
+                log("Ignoring because not MMB clicked.");
                 return;
             }
 
-            Network.CHANNEL.sendToServer(new Message("Sorting inventory!"));
-//            var screen = event.getGui();
-//
-//            if (canSortInventoryScreen(screen))
-//            {
-//                StaticKeyHandler.sortInventory();
-//                return;
-//            }
-//
-//            if (canSortChestScreen(screen))
-//            {
-//                StaticKeyHandler.sortInventory();
-//                return;
-//            }
+            var screen = event.getGui();
+
+            if (didUserClickOnInventorySlot(screen)) {
+                var sortInventoryMessage = new Message(SortTargets.INVENTORY);
+                Network.CHANNEL.sendToServer(sortInventoryMessage);
+                return;
+            }
+
+            if (didUserClickOnContainerSlot(screen)) {
+                var sortContainerMessage = new Message(SortTargets.CONTAINER);
+                Network.CHANNEL.sendToServer(sortContainerMessage);
+                return;
+            }
+
+            if (didUserClickOnIronChestModContainerSlot(screen)) {
+                var sortContainerMessage = new Message(SortTargets.CONTAINER);
+                Network.CHANNEL.sendToServer(sortContainerMessage);
+                return;
+            }
+
+            if (StaticKeyHandler._isCtrlHeldDown) {
+                StaticKeyHandler._isCtrlHeldDown = false;
+                logInfoToChatAboutContainerScreen(screen);
+                return;
+            }
         }
 
-        public static boolean canSortInventoryScreen(Screen screen)
-        {
+        private static boolean didUserClickOnInventorySlot(Screen screen) {
             if (!(screen instanceof InventoryScreen inventoryScreen))
             {
-                Log("Ignoring because MMB not clicked in the inventory screen.");
+                log("Ignoring because MMB not clicked in the inventory screen.");
                 return false;
             }
 
             var slot = inventoryScreen.getSlotUnderMouse();
             if (slot == null)
             {
-                Log("Ignoring because MMB not clicked on an inventory slot.");
+                log("Ignoring because MMB not clicked on an inventory slot.");
                 return false;
             }
 
             var slotIndex = slot.getSlotIndex();
             if (slotIndex < InventoryMenu.INV_SLOT_START || slotIndex >= InventoryMenu.INV_SLOT_END)
             {
-                Log("Ignoring because MMB not clicked on a basic inventory slot.");
+                log("Ignoring because MMB not clicked on a basic inventory slot.");
                 return false;
             }
-
-            var message = new Message("Sorting inventory!");
-            Network.CHANNEL.sendToServer(message);
 
             return true;
         }
 
-        public static boolean canSortChestScreen(Screen screen)
-        {
-//            screen.
+        private static boolean didUserClickOnContainerSlot(Screen screen) {
             if (!(screen instanceof ContainerScreen containerScreen))
             {
-                Log("Ignoring because MMB not clicked in a container screen (ex.: Chest).");
+                log("Ignoring because MMB not clicked in a container screen (ex.: Chest, Shulker box).");
                 return false;
             }
 
             var slot = containerScreen.getSlotUnderMouse();
             if (slot == null)
             {
-                Log("Ignoring because MMB not clicked on a slot.");
+                log("Ignoring because MMB not clicked on a slot.");
                 return false;
             }
 
             if (!(slot.container instanceof SimpleContainer))
             {
-                Log("Ignoring because MMB not clicked on a container's slot.");
+                log("Ignoring because MMB not clicked on a container's slot.");
                 return false;
             }
-
-            var message = new Message("Sorting chest!");
-            Network.CHANNEL.sendToServer(message);
 
             return true;
         }
 
-        public static void sortInventory()
-        {
-            // TODO: Implement me!
+        private static boolean didUserClickOnIronChestModContainerSlot(Screen screen) {
+            // This is the best that I could do without adding a hard dependency to the mod itself.
+            return screen.getClass().getSimpleName().equals("IronChestScreen");
         }
 
-        public static void Log(String message)
-        {
+        private static void log(String message) {
             BoopSorterMod.LOGGER.info("[KeyHandler] " + message);
+        }
+
+        private static void logInfoToChatAboutContainerScreen(Screen screen) {
+            var screenClass = screen.getClass();
+
+            var screenSimpleName = screenClass.getSimpleName();
+            var screenName = screenClass.getName();
+            var screenCanonicalName = screenClass.getCanonicalName();
+            var screenPackageName = screenClass.getPackageName();
+            var screenTypeName = screenClass.getTypeName();
+
+            var message = "\n" +
+                    "\u00A75" + "TypeName:\n" + "\u00A7d" + screenTypeName + "\n\n" +
+                    "\u00A74" + "Name:\n" + "\u00A7c" + screenName + "\n\n" +
+                    "\u00A76" + "SimpleName:\n" + "\u00A7e" + screenSimpleName + "\n\n" +
+                    "\u00A72" + "CanonicalName:\n" + "\u00A7a" + screenCanonicalName + "\n\n" +
+                    "\u00A73" + "PackageName:\n" + "\u00A7b" + screenPackageName;
+
+            Minecraft.getInstance().gui.getChat().addMessage(new TextComponent(message));
         }
     }
 }
--- src/main/java/com/example/examplemod/Server.java
+++ src/main/java/com/example/examplemod/Server.java
@@ -3,7 +3,6 @@ package com.example.examplemod;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -15,9 +14,8 @@ import java.util.function.Supplier;
 public class Server {
     public static void onMessage(Message message, Supplier<NetworkEvent.Context> contextSupplier) {
         var context = contextSupplier.get();
-
-        var source = message.getSource();
-        BoopSorterMod.LOGGER.info("[Server] " + source);
+        var sortTarget = message.getSortTarget();
+        log("User requested to sort: " + sortTarget);
 
         context.enqueueWork(() -> {
             var player = context.getSender();
@@ -25,131 +23,149 @@ public class Server {
                 return;
             }
 
-            mergeStacksInContainer(player.containerMenu);
-//            var containerSlotsIndexStart = 0;
-//            var containerSlotsIndexEnd = containerSlots.size() - Inventory.INVENTORY_SIZE;
-//            for (var i = containerSlotsIndexStart; i < containerSlotsIndexEnd; i++) {
-//                var slot
-//            }
-//
-//            var chestIndexStart = 0;
-//            var chestIndexStartEnd = player.containerMenu.slots.size() - Inventory.INVENTORY_SIZE;
-//            log("lastIndex: " + chestIndexStartEnd);
-
-//            var slots = container.slots;
-//            slots.forEach(slot -> {
-//                var item = slot.getItem();
-//                if (item.getDescriptionId().equals(Items.AIR.getDescriptionId())) {
-//                    return;
-//                }
-//                log(item.toString());
-//            });
-
-//            log("----------------------");
-//
-//            var containerItems = container.getItems();
-//            containerItems.forEach(itemStack -> {
-//                if (itemStack.getDescriptionId().equals(Items.AIR.getDescriptionId())) {
-//                    return;
-//                }
-//
-//                log(itemStack.toString());
-//            });
-
-//            var inventory = player.getInventory();
-//            var airItemId = Items.AIR.getDescriptionId();
-//            var itemsInInventory = new HashMap<String, List<ItemStack>>();
-//
-//            for (var i = InventoryMenu.INV_SLOT_START; i < InventoryMenu.INV_SLOT_END; i++) {
-//                var itemStack = inventory.getItem(i);
-//                var itemId = itemStack.getDescriptionId();
-//
-//                if (itemId.equals(airItemId)) {
-//                    // Ignore empty slots
-//                    continue;
-//                }
-//
-//                if (!itemStack.isStackable()) {
-//                    continue;
-//                }
-//
-//                if (itemStack.getCount() == itemStack.getMaxStackSize()) {
-//                    // Ignore maxed out stacks
-//                    continue;
-//                }
-//
-//                if (itemsInInventory.containsKey(itemId)) {
-//                    var itemStacks = itemsInInventory.get(itemId);
-//                    itemStacks.add(itemStack);
-//                    itemsInInventory.put(itemId, itemStacks);
-//                    continue;
-//                }
-//
-//                var itemStacks = new ArrayList<ItemStack>();
-//                itemStacks.add(itemStack);
-//                itemsInInventory.put(itemId, itemStacks);
-//            }
-//
-//            // Ignore single stacks
-//            itemsInInventory.entrySet().removeIf((entry) -> entry.getValue().size() <= 1);
-//
-//            itemsInInventory.forEach((itemId, itemStacks) -> {
-//                int totalItemCount = itemStacks.stream().map(ItemStack::getCount).reduce(Integer::sum).orElse(0);
-//
-//                for (ItemStack itemStack: itemStacks) {
-//                    var maxSize = itemStack.getMaxStackSize();
-//                    if (totalItemCount >= maxSize) {
-//                        itemStack.setCount(maxSize);
-//                        totalItemCount -= maxSize;
-//                        continue;
-//                    }
-//
-//                    if (totalItemCount > 0) {
-//                        itemStack.setCount(totalItemCount);
-//                        totalItemCount = 0;
-//                        continue;
-//                    }
-//
-//                    inventory.removeItem(itemStack);
-//                }
-//            });
-//
-//            var inventoryItems = new ArrayList<ItemStack>();
-//
-//            for (var i = InventoryMenu.INV_SLOT_START; i < InventoryMenu.INV_SLOT_END; i++) {
-//                var itemStack = inventory.getItem(i);
-//                var itemStackId = itemStack.getDescriptionId();
-//                if (itemStackId.equals(airItemId)) {
-//                    continue;
-//                }
-//
-//                inventoryItems.add(itemStack);
-//            }
-//
-//            inventoryItems.sort((item1, item2) -> {
-//                var item1Id = item1.getDescriptionId();
-//                var item2Id = item2.getDescriptionId();
-//                // var item1IdReversed = new StringBuilder(item1Id).reverse().toString();
-//                // var item2IdReversed = new StringBuilder(item2Id).reverse().toString();
-//                return item1IdReversed.compareTo(item2IdReversed);
-//            });
-//
-//            var slotIndex = InventoryMenu.INV_SLOT_START;
-//            for (ItemStack inventoryItem : inventoryItems) {
-//                var inventoryItemCopy = inventoryItem.copy();
-//                inventory.setItem(slotIndex, inventoryItemCopy);
-//                inventory.removeItem(inventoryItem);
-//                slotIndex++;
-//            }
+            switch (sortTarget) {
+                case INVENTORY -> mergeAndSortInventoryItemStacks(player.getInventory());
+                case CONTAINER -> mergeAndSortContainerItemStacks(player.containerMenu);
+                default -> log("Unknown value of enum 'SortTarget'!");
+            }
         });
         context.setPacketHandled(true);
     }
 
     private static void log(String message) {
-        BoopSorterMod.LOGGER.info(message);
+        BoopSorterMod.LOGGER.info("[Server] " + message);
     }
 
-    private static void mergeStacksInContainer(AbstractContainerMenu containerMenu) {
+    // TODO: Explain me
+    private static void mergeInventoryItemStacks(Inventory inventory) {
+        var airItemId = Items.AIR.getDescriptionId();
+        var itemsInInventory = new HashMap<String, List<ItemStack>>();
+
+        for (var i = InventoryMenu.INV_SLOT_START; i < InventoryMenu.INV_SLOT_END; i++) {
+            var itemStack = inventory.getItem(i);
+            var itemId = itemStack.getDescriptionId();
+
+            if (itemId.equals(airItemId)) {
+                // Ignore empty slots
+                continue;
+            }
+
+            if (!itemStack.isStackable()) {
+                continue;
+            }
+
+            if (itemStack.getCount() == itemStack.getMaxStackSize()) {
+                // Ignore maxed out stacks
+                continue;
+            }
+
+            if (itemsInInventory.containsKey(itemId)) {
+                var itemStacks = itemsInInventory.get(itemId);
+                itemStacks.add(itemStack);
+                itemsInInventory.put(itemId, itemStacks);
+                continue;
+            }
+
+            var itemStacks = new ArrayList<ItemStack>();
+            itemStacks.add(itemStack);
+            itemsInInventory.put(itemId, itemStacks);
+        }
+
+        // Ignore single stacks
+        itemsInInventory.entrySet().removeIf((entry) -> entry.getValue().size() <= 1);
+
+        itemsInInventory.forEach((itemId, itemStacks) -> {
+            int totalItemCount = itemStacks.stream().map(ItemStack::getCount).reduce(Integer::sum).orElse(0);
+
+            for (ItemStack itemStack: itemStacks) {
+                var maxSize = itemStack.getMaxStackSize();
+                if (totalItemCount >= maxSize) {
+                    itemStack.setCount(maxSize);
+                    totalItemCount -= maxSize;
+                    continue;
+                }
+
+                if (totalItemCount > 0) {
+                    itemStack.setCount(totalItemCount);
+                    totalItemCount = 0;
+                    continue;
+                }
+
+                inventory.removeItem(itemStack);
+            }
+        });
+    }
+
+    // TODO: Explain me
+    private static void sortInventoryItemStacks(Inventory inventory) {
+        var airItemId = Items.AIR.getDescriptionId();
+        var inventoryItems = new ArrayList<ItemStack>();
+
+        // TODO: What does this loop do?
+        for (var i = InventoryMenu.INV_SLOT_START; i < InventoryMenu.INV_SLOT_END; i++) {
+            var itemStack = inventory.getItem(i);
+            var itemStackId = itemStack.getDescriptionId();
+            if (itemStackId.equals(airItemId)) {
+                continue;
+            }
+
+            inventoryItems.add(itemStack);
+        }
+
+        // TODO: Add an example
+        inventoryItems.sort((itemStack1, itemStack2) -> {
+            // TODO: Simplify
+            var item1Name = "";
+            var item1 = itemStack1.getItem();
+            if (item1.getRegistryName() == null) {
+                item1Name = item1.getDescriptionId();
+            } else {
+                item1Name = item1.getRegistryName().getPath();
+            }
+
+            var item2Name = "";
+            var item2 = itemStack2.getItem();
+            if (item2.getRegistryName() == null) {
+                item2Name = item2.getDescriptionId();
+            } else {
+                item2Name = item2.getRegistryName().getPath();
+            }
+
+            return item1Name.compareTo(item2Name);
+        });
+
+        // TODO: What does this loop do?
+        var slotIndex = InventoryMenu.INV_SLOT_START;
+        for (ItemStack inventoryItem : inventoryItems) {
+            var inventoryItemCopy = inventoryItem.copy();
+            inventory.setItem(slotIndex, inventoryItemCopy);
+            inventory.removeItem(inventoryItem);
+            slotIndex++;
+        }
+    }
+
+    private static void mergeAndSortInventoryItemStacks(Inventory inventory) {
+        if (inventory == null) {
+            log("Attempted to sort the inventory but it was `null`!");
+            return;
+        }
+
+        mergeInventoryItemStacks(inventory);
+        sortInventoryItemStacks(inventory);
+    }
+
+    private static void mergeAndSortContainerItemStacks(AbstractContainerMenu containerMenu) {
+        if (containerMenu == null) {
+            log("Attempted to sort the container but it was `null`!");
+            return;
+        }
+
+        mergeContainerItemStacks(containerMenu);
+        sortContainerItemStacks(containerMenu);
+    }
+
+    private static void mergeContainerItemStacks(AbstractContainerMenu containerMenu) {
+        // TODO: Duplicated - move to parent function?
         var containerSlots = containerMenu.slots;
         var containerIndexStart = 0;
         var containerIndexEnd = containerSlots.size() - Inventory.INVENTORY_SIZE;
@@ -217,15 +233,16 @@ public class Server {
                 itemStack.setCount(0);
             }
         });
+    }
 
+    private static void sortContainerItemStacks(AbstractContainerMenu containerMenu) {
+        // TODO: Duplicated - move to parent function?
+        var containerSlots = containerMenu.slots;
+        var containerIndexStart = 0;
+        var containerIndexEnd = containerSlots.size() - Inventory.INVENTORY_SIZE;
 
-
-
-        // ---------------
-        // --- SORTING ---
-        // ---------------
+        var airItemId = Items.AIR.getDescriptionId();
         var itemCopies = new ArrayList<ItemStack>();
-//        var notEmptyContainerSlots = new ArrayList<Slot>();
 
         for (var i = containerIndexStart; i < containerIndexEnd; i++) {
             var slot = containerSlots.get(i);
@@ -236,22 +253,13 @@ public class Server {
             }
 
             itemCopies.add(itemStack.copy());
-//            notEmptyContainerSlots.add(slot);
         }
 
         if (itemCopies.size() < 1) {
             return;
         }
 
-//        notEmptyContainerSlots.sort((slot1, slot2) -> {
-//            var item1Id = slot1.getItem().getDescriptionId();
-//            var item2Id = slot2.getItem().getDescriptionId();
-//            // var item1IdReversed = new StringBuilder(item1Id).reverse().toString();
-//            // var item2IdReversed = new StringBuilder(item2Id).reverse().toString();
-//            return item1Id.compareTo(item2Id);
-//        });
-
-//        itemCopies.sort(Comparator.comparing(ItemStack::getDescriptionId));
+        // TODO: Add an example
         itemCopies.sort((itemStack1, itemStack2) -> {
             // TODO: Simplify
             var item1Name = "";
@@ -269,8 +277,8 @@ public class Server {
             } else {
                 item2Name = item2.getRegistryName().getPath();
             }
-            var result = item1Name.compareTo(item2Name);
-            return result;
+
+            return item1Name.compareTo(item2Name);
         });
 
         var index = containerIndexStart;
@@ -285,20 +293,5 @@ public class Server {
             var slot = containerSlots.get(i);
             slot.getItem().setCount(0);
         }
-
-//        var slotIndex = containerIndexStart;
-//        for (var i = containerIndexStart; i < containerIndexEnd; i++) {
-//            var slot = containerSlots.get(i);
-//            // TODO: Does this remove a dangling pointer is this useless?
-//            slot.getItem().setCount(0);
-//            slot.set();
-//        }
-//        for (Slot slot : notEmptyContainerSlots) {
-//            var item = slot.getItem();
-//            var itemCopy = item.copy();
-//            inventory.setItem(slotIndex, inventoryItemCopy);
-//            inventory.removeItem(inventoryItem);
-//            slotIndex++;
-//        }
     }
 }
